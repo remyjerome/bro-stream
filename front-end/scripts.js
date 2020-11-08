61ab28c35bfa53f4abd6a464057bc1929bcb8ecb
Goldawn <heunow@gmail.com>
2020-11-08 18:38:38 +0100

    fix(inputs): fixed the triggering of the events

--- front-end/scripts.js
+++ front-end/scripts.js
@@ -7,65 +7,69 @@ const speedBar = document.querySelector('[name="playbackRate"]');
 const inputs = document.querySelectorAll('[type=range]');
 const fillProgress = document.querySelector('.progress__filled');
 let play = false;
-let progress;
-let fillBar = 0;
-video.preload = "auto";
-const duration = String(video.duration);
-document.querySelector('[name="currentTime"]').setAttribute('max', `${duration}`);
+let click;
 
-videoProgress();
+video.onloadedmetadata = function () {
 
-function playPause() {
-    if (!play) {
-        video.play();
-        playButton.innerHTML="||";
-        play = true;
-        progress = setInterval(videoProgress, 100);
+    video.preload = "auto";
+    const duration = String(video.duration);
+    document.querySelector('[name="currentTime"]').setAttribute('max', `${duration}`);
+
+    function playPause() {
+        if (!play) {
+            video.play();
+            playButton.innerHTML="||";
+            play = true;
+        }
+        else {
+            video.pause();
+            playButton.innerHTML="►";
+            play = false;
+        }
     }
-    else {
-        video.pause();
-        playButton.innerHTML="►";
-        play = false;
-        stopProgress();
+
+    function backwardVid() {
+        video.currentTime = video.currentTime -10;
     }
-}
 
-function backwardVid() {
-    video.currentTime = video.currentTime -10;
-    videoProgress();
-}
-
-function forwardVid() {
-    video.currentTime = video.currentTime +25;
-    videoProgress();
-}
-
-function videoProgress() {
-    fillBar = video.currentTime === 0 ? video.currentTime : video.currentTime*100/video.duration;
-    fillProgress.style.flexBasis=`${fillBar}`+"%";
-    if (fillBar == 100) {
-        play = false;
-        playButton.innerHTML="►";
-        stopProgress();
+    function forwardVid() {
+        video.currentTime = video.currentTime +25;
     }
+
+    function stopProgress() {
+        clearInterval(progress);
+    }
+
+    function triggerMouseMoveEvent() {
+        click = true;
+    }
+
+    function stopMouseMoveEvent() {
+        click = false;
+    }
+
+    function handleOnChangeInputs() {
+        console.log(this.name, this.value);
+        video[this.name] = this.value;
+    }
+
+    function handleOnClickInputs() {
+        if (click) {
+            console.log(this.name, this.value);
+            video[this.name] = this.value;
+        }
+    }
+
+    
+    
+    playButton.addEventListener('click', playPause);
+    backwardButton.addEventListener('click', backwardVid);
+    forwardButton.addEventListener('click', forwardVid);
+    inputs.forEach(input => input.addEventListener('mousedown', triggerMouseMoveEvent));
+    inputs.forEach(input => input.addEventListener('mouseup', stopMouseMoveEvent));
+    inputs.forEach(input => input.addEventListener('change', handleOnChangeInputs));
+    inputs.forEach(input => input.addEventListener('mousemove', handleOnClickInputs));
+    video.addEventListener('click', playPause);
+
 }
-
-function stopProgress() {
-    clearInterval(progress);
-}
-
-function handleInputs() {
-    console.log(this.name, this.value);
-    video[this.name] = this.value;
-}
-
-playButton.addEventListener('click', playPause);
-backwardButton.addEventListener('click', backwardVid);
-forwardButton.addEventListener('click', forwardVid);
-inputs.forEach(input => input.addEventListener('change', handleInputs));
-inputs.forEach(input => input.addEventListener('mousemove', handleInputs));
-video.addEventListener('click', playPause);
-
-
-
 
